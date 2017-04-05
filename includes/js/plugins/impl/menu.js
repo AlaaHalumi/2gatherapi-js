779a693408d348cf2412b35c5f304ac5558a90ed
niv90 <nivb1990@gmail.com>
2017-04-05 21:23:43 +0300

    menu object work . need to create factory

---
+++ includes/js/plugins/impl/menu.js
@@ -0,0 +1,38 @@
+class Menu extends Object{
+
+    constructor(domElement){
+        super();
+        this.domElement = domElement;
+        this.menuFactory = new MenuFactory();
+    }
+
+    draw(){
+        let options = eval(this.domElement.getAttribute("options"));
+        // this.domElement.innerHTML += "<ul></ul>";
+
+        var ul = document.createElement("ul");
+
+        for(var propertyName in options) {
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+
+            for(var propertyValue in options[propertyName]) {
+                let value = propertyValue;
+                if(value == "text"){
+                    a.innerHTML = options[propertyName][value];
+                }
+                else{
+                    a.setAttribute(value,options[propertyName][value]);
+                }
+
+            }
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+        this.domElement.appendChild(ul);
+        // let link = this.domElement.getElementsByTagName("tg-ul");
+        // for(let i = 0; i < inputs.length; i++){
+        //     this.inputFactory.createObject(inputs[i]);
+        // }
+    }
+}
