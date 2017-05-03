a8b7135487f83a3caa227ef35e3e819348c9338b
niv90 <nivb1990@gmail.com>
2017-05-03 08:18:24 +0300

    after scanner

--- includes/js/2gatherapi.js
+++ includes/js/2gatherapi.js
@@ -2,9 +2,14 @@
 
 class Gatherapi {
     constructor(options) {
+
         // if(options.middlewareDevices) {
         //     this.middleware = new Middleware(options.middlewareDevices, options.middleInterval ? options.middleInterval : 10000);
         // }
+
+        this.objectToObjectFactoryMap = {inputText:"inputFactory"};
+        this.pluginToPluginFactoryMap = {loginFactory:"loginFactory"};
+
         this.utils = {
             annyangUtil : new AnnyangUtil(),
             chatUtil  : new ChatUtil(),
@@ -20,11 +25,35 @@ class Gatherapi {
              menuFactory: new MenuFactory(), paragraphFactory: new ParagraphFactory(), libraryFactory: new LibraryFactory(),
             accessibilityFactory : new AccessibilityFactory() };
         this.utilsConfiguration(options);
+        // this.scanForPluginsOrObjects();
     }
 
     utilsConfiguration(options){
         // this.utils.annyangUtil.setLanguage(options["voiceToTextLanguage"]);
         // this.utils.textToVoice.setLanguage(options["textToVoice"]);
         // this.utils.circleObject.initCircleObject();
+    }
+
+    scanForPluginsOrObjects(){
+        this.scanPlugins();
+        this.scanObjects();
+    }
+
+    scanObjects(){
+        for(var objectToObjectFactoryKey in this.objectToObjectFactoryMap){
+            var elements = document.getElementsByTagName(objectToObjectFactoryKey);
+            for(var element in elements){
+                this.objectFactories[this.objectToObjectFactoryMap[objectToObjectFactoryKey]].createObject(element);
+            }
+        }
+    }
+
+    scanPlugins(){
+        for(var pluginToPluginFactoryKey in this.pluginToPluginFactoryMap){
+            var elements = document.getElementsByTagName(pluginToPluginFactoryKey);
+            for(var element in elements){
+                this.pluginFactories[this.pluginToPluginFactoryMap[pluginToPluginFactoryKey]].createPlugin(element);
+            }
+        }
     }
 }
