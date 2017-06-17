49ed7afd0b0295e20c4256b6958df57d365f1377
ysapir <ysapir@ebay.com>
2017-06-17 13:28:20 +0300

    game plugin

--- includes/js/plugins/impl/game.js
+++ includes/js/plugins/impl/game.js
@@ -1,159 +1,84 @@
-class Game extends Object {
+class Game extends Plugin {
 
     constructor(domElement) {
         super();
-        this.domElement = domElement;
-        this.linkFactory = new LinkFactory();
-        this.questions = {
-            "questions": [
-                {
-                    "question": "Which picture shows Shrek?",
-                    "choices": ["shrek", "pooh", "dog", "thing"],
-                    "correctAnswer": 0
-                },
-                {
-                    "question": "Which picture shows an Owl?",
-                    "choices": ["Penguin", "Flamingo", "Owl", "Quetzal"],
-                    "correctAnswer": 2
-                },
-                {
-                    "question": "Which picture shows a fruit?",
-                    "choices": ["Brocoli", "Carrots", "Apple", "Paprika"],
-                    "correctAnswer": 2
-                },
-                {
-                    "question": "Which picture shows the answer of 2+2?",
-                    "choices": ["8", "4", "6", "5"],
-                    "correctAnswer": 1
-                },
-                {
-                    "question": "Which picture shows a Quetzal?",
-                    "choices": ["8", "4", "6", "5"],
-                    "correctAnswer": 2
-                }
-            ]
-        }
-
-        this.questionCounter = 0; //Tracks question number
-        this.selections = []; //Array containing user choices
-        // var quiz = document.getElementById("quiz"); //Quiz div object
+        console.log("it's game");
+        this.questionCounter = 0;
+        this.selections = [];
         this.questions;
+        $(domElement).append("<div id='quiz'></div>");
         this.quiz = $('#quiz');
-
     }
 
     draw() {
-
-        //main container
-        var divQuiz = document.createElement("div");
-        divQuiz.setAttribute("id", "quiz");
-        this.domElement.appendChild(divQuiz);
-
-
-        var divNext = document.createElement("div");
-        divNext.setAttribute("class", "buttonGame");
-        divNext.setAttribute("id", "next");
-        this.domElement.appendChild(divNext);
-
-
-        var divPrev = document.createElement("div");
-        divPrev.setAttribute("class", "buttonGame");
-        divPrev.setAttribute("id", "prev");
-        this.domElement.appendChild(divPrev);
-
-        var divStart = document.createElement("div");
-        divStart.setAttribute("class", "buttonGame");
-        divStart.setAttribute("id", "start");
-        this.domElement.appendChild(divStart);
-
-        let aNext = document.createElement("tg-a");
-        divNext.appendChild(aNext);
-        let nextLink = {
-            link: {
-                href: "#",
-                text: "Next"
-            }
-        }
-        this.linkFactory.createObject(aNext, nextLink);
-
-        let aPrev = document.createElement("tg-a")
-        divPrev.appendChild(aPrev);
-        let prevLink = {
-            link: {
-                href: "#",
-                text: "Prev"
-            }
-        }
-        this.linkFactory.createObject(aPrev, prevLink);
-
-        let aStart = document.createElement("tg-a")
-        divStart.appendChild(aStart);
-        let startLink = {
-            link: {
-                href: "#",
-                text: "Start Over"
-            }
-        }
-        this.linkFactory.createObject(aStart, startLink);
-
-        this.displayNext();
-
-        // Click handler for the 'next' button
+        this.checkUser();
+        var self = this;
         $('#next').on('click', function (e) {
             e.preventDefault();
 
-            // Suspend click listener during fade animation
-            if (this.quiz.is(':animated')) {
+            if (self.quiz.is(':animated')) {
                 return false;
             }
-            this.choose();
+            self.choose();
 
-            // If no user selection, progress is stopped
-            if (isNaN(this.selections[this.questionCounter])) {
+            if (isNaN(self.selections[self.questionCounter])) {
                 alert('Please make a selection!');
             } else {
-                this.questionCounter++;
-                this.displayNext();
+                self.questionCounter++;
+                self.displayNext();
             }
         });
 
-        // Click handler for the 'prev' button
         $('#prev').on('click', function (e) {
             e.preventDefault();
 
-            if (this.quiz.is(':animated')) {
+            if (self.quiz.is(':animated')) {
                 return false;
             }
-            this.choose();
-            this.questionCounter--;
-            this.displayNext();
+            self.choose();
+            self.questionCounter--;
+            self.displayNext();
         });
 
-        // Click handler for the 'Start Over' button
         $('#start').on('click', function (e) {
             e.preventDefault();
 
-            if (this.quiz.is(':animated')) {
+            if (self.quiz.is(':animated')) {
                 return false;
             }
-            this.questionCounter = 0;
-            this.selections = [];
-            this.displayNext();
+            self.questionCounter = 0;
+            self.selections = [];
+            self.displayNext();
             $('#start').hide();
         });
 
-        // Animates buttons on hover
         $('.buttonGame').on('mouseenter', function () {
             $(this).addClass('active');
         });
         $('.buttonGame').on('mouseleave', function () {
             $(this).removeClass('active');
         });
-
-
-        // // Creates and returns the div that contains the questions and
-        // // the answer selections
     }
+
+    checkUser() {
+        console.log("inside checkUser");
+        var url = 'tsconfig.json';
+        var jsonData;
+        var xhttp = new XMLHttpRequest();
+        var self = this;
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                jsonData = JSON.parse(this.responseText);
+                console.log(jsonData);
+                console.log(jsonData["questions"].length);
+                console.log(jsonData["questions"]);
+                self.questions = jsonData["questions"];
+                self.displayNext();
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    };
 
     createQuestionElement(index) {
         var qElement = $('<div>', {
@@ -168,75 +93,51 @@ class Game extends Object {
         var radioButtons = this.createRadios(index);
         qElement.append(radioButtons);
 
+
         return qElement;
     }
 
-    // // Creates a list of the answer choices as radio inputs
     createRadios(index) {
 
         var radioList = $('<div>', {
             class: 'row'
         });
-
         var input = '';
         // var item = '';
         for (var i = 0; i < this.questions[index].choices.length; i++) {
-
             input = '<label class="cell"><input type="radio" name="answer" value=' + i + ' /> ' + (i + 1) + "." + "   " + '<img src="images/quez/' + this.questions[index].choices[i] + "." + "jpg" + '" width="40%" alt=' + this.questions[index].choices[i] + ' /></label>';
-            // item = '<img src="images/quez/'+questions[index].choices[i]+"."+"jpg"+'" width="40%" alt='+questions[index].choices[i]+' />';
-            // // input += item;
-            // input.append(item);
             radioList.append(input);
         }
-
         return radioList;
     }
 
-    //
-    // // Reads the user selection and pushes the value to an array
     choose() {
         this.selections[this.questionCounter] = +$('input[name="answer"]:checked').val();
     }
 
-
-    // Computes score and returns a paragraph element to be displayed
-    displayScore() {
-        var score = $('<p>', {class: 'score'});
-
-        var numCorrect = 0;
-        for (var i = 0; i < this.selections.length; i++) {
-            if (this.selections[i] === this.questions[i].correctAnswer) {
-                numCorrect++;
-            }
-        }
-
-        score.append('You got ' + numCorrect + ' questions out of ' +
-            this.questions.length + ' right!!!');
-        return score;
-    }
-
     displayNext() {
+        var self = this;
         this.quiz.fadeOut(function () {
             $('#question').remove();
 
-            if (this.questionCounter < this.questions.length) {
-                var nextQuestion = this.createQuestionElement(questionCounter);
-                this.quiz.append(nextQuestion).fadeIn();
-                if (!(isNaN(this.selections[this.questionCounter]))) {
-                    $('input[value=' + this.selections[this.questionCounter] + ']').prop('checked', true);
+            if (self.questionCounter < self.questions.length) {
+                var nextQuestion = self.createQuestionElement(self.questionCounter);
+                self.quiz.append(nextQuestion).fadeIn();
+                if (!(isNaN(self.selections[self.questionCounter]))) {
+                    $('input[value=' + self.selections[self.questionCounter] + ']').prop('checked', true);
                 }
 
                 // Controls display of 'prev' button
-                if (this.questionCounter === 1) {
+                if (self.questionCounter === 1) {
                     $('#prev').show();
-                } else if (this.questionCounter === 0) {
+                } else if (self.questionCounter === 0) {
 
                     $('#prev').hide();
                     $('#next').show();
                 }
             } else {
-                var scoreElem = this.displayScore();
-                this.quiz.append(scoreElem).fadeIn();
+                var scoreElem = self.displayScore();
+                self.quiz.append(scoreElem).fadeIn();
                 $('#next').hide();
                 $('#prev').hide();
                 $('#start').show();
@@ -244,4 +145,16 @@ class Game extends Object {
         });
     }
 
+    displayScore() {
+        var score = $('<p>', {class: 'score'});
+        var numCorrect = 0;
+        for (var i = 0; i < this.selections.length; i++) {
+            if (this.selections[i] === this.questions[i].correctAnswer) {
+                numCorrect++;
+            }
+        }
+        score.append('You got ' + numCorrect + ' questions out of ' +
+            this.questions.length + ' right!!!');
+        return score;
+    }
 }
